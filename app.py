18998345a6b18f5567a95756605a29cb5af31c69
Jordan Manning <jorjacman@Jordans-MBP.hsd1.va.comcast.net>
2023-11-20 10:20:39 -0500

    test

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-st.title("testing")
+# Create an instance of the FastAPI class
+app = FastAPI()
 
+# Define a POST endpoint
+@app.post("/hello")
+def hello_world():
+    # Return a greeting with the provided name
+    return {"message": "hello"}
 
+# Run the application
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
